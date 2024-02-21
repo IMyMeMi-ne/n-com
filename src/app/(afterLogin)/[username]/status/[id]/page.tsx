f9f51d38982083390f845803e876354ef2540cde
Namhyun <jenple9709@gmail.com>
2024-02-21 15:34:35 +0900

    feat: Implement navigation to post detail page on post click

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return null;
+  return <div>상세페이지</div>;
 }
--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -5,11 +5,13 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
 import ActionButtons from "./ActionButtons";
+import PostArticle from "./PostArticle";
 dayjs.extend(relativeTime);
 dayjs.locale("ko");
 
 export default function Post() {
   const target = {
+    postId: 1,
     User: {
       id: "namhyun",
       nickname: "iMyMeMine",
@@ -20,7 +22,7 @@ export default function Post() {
     Images: [],
   };
   return (
-    <article className={style.post}>
+    <PostArticle post={target}>
       <div className={style.postWrapper}>
         <div className={style.postUserSection}>
           <Link href={`/${target.User.id}`} className={style.postUserImage}>
@@ -49,6 +51,6 @@ export default function Post() {
           <ActionButtons />
         </div>
       </div>
-    </article>
+    </PostArticle>
   );
 }
