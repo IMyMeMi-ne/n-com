6b3d668a8e31ff0bbfa491c0fd0b093a0cdb9713
Namhyun <jenple9709@gmail.com>
2024-02-21 15:45:23 +0900

    design: Comment form

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <div>상세페이지</div>;
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import style from "./singPost.module.css";
+import Post from "@/app/(afterLogin)/_component/Post";
+import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm";
+
+export default function SinglePost() {
+  return (
+    <div className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>게시하기</h3>
+      </div>
+      <Post />
+      <CommentForm />
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
