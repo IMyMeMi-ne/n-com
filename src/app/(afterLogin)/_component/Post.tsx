4ee8388e8a1c626d5e0c9834ec37d95d3f6b2ec9
Namhyun <jenple9709@gmail.com>
2024-02-22 23:05:31 +0900

    design: Post-images-section

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -28,7 +28,12 @@ export default function Post({ noImage }: Props = {}) {
     Images: [] as any[],
   };
   if (Math.random() > 0.5 && !noImage) {
-    target.Images.push({ imageId: 1, link: faker.image.urlLoremFlickr() });
+    target.Images.push(
+      { imageId: 1, link: faker.image.urlLoremFlickr() },
+      { imageId: 2, link: faker.image.urlLoremFlickr() },
+      { imageId: 3, link: faker.image.urlLoremFlickr() },
+      { imageId: 3, link: faker.image.urlLoremFlickr() }
+    );
   }
   return (
     <PostArticle post={target}>
@@ -56,15 +61,8 @@ export default function Post({ noImage }: Props = {}) {
             </span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}>
-            {target.Images?.length > 0 && (
-              <Link
-                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
-                className={style.postImageSection}
-              >
-                <img src={target.Images[0]?.link} alt="" />
-              </Link>
-            )}
+          <div>
+            <PostImages post={target} />
           </div>
           <ActionButtons />
         </div>
--- src/app/(afterLogin)/_component/PostImages.tsx
+++ src/app/(afterLogin)/_component/PostImages.tsx
@@ -1,3 +1,133 @@
-export default function PostImages() {
-  return;
+import style from "./post.module.css";
+import Link from "next/link";
+import { ReactNode } from "react";
+import cx from "classnames";
+type Props = {
+  children?: ReactNode;
+  post: {
+    postId: number;
+    content: string;
+    User: {
+      id: string;
+      nickname: string;
+      image: string;
+    };
+    createdAt: Date;
+    Images: any[];
+  };
+};
+
+export default function PostImages({ post }: Props) {
+  if (!post.Images) return null;
+  if (!post.Images.length) return null;
+  if (post.Images.length === 1) {
+    return (
+      post.Images?.length > 0 && (
+        <Link
+          href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+          className={cx(style.postImageSection, style.oneImage)}
+          style={{
+            backgroundImage: `url(${post.Images[0]?.link})`,
+            backgroundSize: "cover",
+          }}
+        >
+          <img src={post.Images[0]?.link} alt="" />
+        </Link>
+      )
+    );
+  }
+  if (post.Images.length === 2) {
+    return (
+      post.Images?.length > 0 && (
+        <div className={cx(style.postImageSection, style.twoImages)}>
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+            style={{
+              backgroundImage: `url(${post.Images[0]?.link})`,
+              backgroundSize: "cover",
+            }}
+          />
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[1].imageId}`}
+            style={{
+              backgroundImage: `url(${post.Images[1]?.link})`,
+              backgroundSize: "cover",
+            }}
+          />
+        </div>
+      )
+    );
+  }
+  if (post.Images.length === 3) {
+    return (
+      post.Images?.length > 0 && (
+        <div className={cx(style.postImageSection, style.threeImages)}>
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+            style={{
+              width: "100%",
+              height: "100%",
+              backgroundImage: `url(${post.Images[0]?.link})`,
+              backgroundSize: "cover",
+            }}
+          />
+          <div>
+            <Link
+              href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[1].imageId}`}
+              style={{
+                height: "50%",
+                backgroundImage: `url(${post.Images[1]?.link})`,
+                backgroundSize: "cover",
+              }}
+            />
+            <Link
+              href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[2].imageId}`}
+              style={{
+                height: "50%",
+                backgroundImage: `url(${post.Images[2]?.link})`,
+                backgroundSize: "cover",
+              }}
+            />
+          </div>
+        </div>
+      )
+    );
+  }
+  if (post.Images.length === 4) {
+    return (
+      post.Images?.length > 0 && (
+        <div className={cx(style.postImageSection, style.fourImages)}>
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[0].imageId}`}
+            style={{
+              backgroundImage: `url(${post.Images[0]?.link})`,
+              backgroundSize: "cover",
+            }}
+          />
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[1].imageId}`}
+            style={{
+              backgroundImage: `url(${post.Images[1]?.link})`,
+              backgroundSize: "cover",
+            }}
+          />
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[2].imageId}`}
+            style={{
+              backgroundImage: `url(${post.Images[2]?.link})`,
+              backgroundSize: "cover",
+            }}
+          />
+          <Link
+            href={`/${post.User.id}/status/${post.postId}/photo/${post.Images[3].imageId}`}
+            style={{
+              backgroundImage: `url(${post.Images[3]?.link})`,
+              backgroundSize: "cover",
+            }}
+          />
+        </div>
+      )
+    );
+  }
+  return null;
 }
