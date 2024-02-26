31c0de821cca162d08eaa2dec68ece080cbd1752
Namhyun <jenple9709@gmail.com>
2024-02-26 14:26:57 +0900

    feat: MSWComponent

---
+++ src/app/_component/MSWComponent.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { useEffect } from 'react';
+export const MSWComponent = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (process.env.NEXT_PUBLIC_MOCKING === 'enabled') {
+        require('@/mocks/browser');
+      }
+    }
+  }, []);
+  return null;
+};
