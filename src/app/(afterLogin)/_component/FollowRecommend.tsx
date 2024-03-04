e9a5f032eb5031612010e86a914c2cce2a066c4e
Namhyun <jenple9709@gmail.com>
2024-03-04 16:13:14 +0900

    feat: Change trend data display

--- src/app/(afterLogin)/_component/FollowRecommend.tsx
+++ src/app/(afterLogin)/_component/FollowRecommend.tsx
@@ -2,33 +2,22 @@
 import style from './followRecommend.module.css';
 import Image from 'next/image';
 import FollowButton from './FollowButton';
-
-type fakeUserProps = {
-  id: string;
-  hashTag: string;
-  image: any;
+import { User } from '@/app/model/User';
+type Props = {
+  user: User;
 };
-export default function FollowRecommend() {
-  const fakeUser: fakeUserProps = {
-    id: 'iMyMemine',
-    hashTag: '@namhyun',
-    image: '/darkXLogo.jpg',
-  };
+
+export default function FollowRecommend({ user }: Props) {
   return (
     <div className={style.container}>
       <div className={style.userLogoSection}>
         <div className={style.userLogo}>
-          <Image
-            src={fakeUser.image}
-            width={40}
-            height={40}
-            alt={fakeUser.id}
-          />
+          <Image src={user.image} width={40} height={40} alt={user.id} />
         </div>
       </div>
       <div className={style.userInfo}>
-        <div className={style.userId}>{fakeUser.id}</div>
-        <div className={style.userHashTag}>{fakeUser.hashTag}</div>
+        <div className={style.userId}>{user.id}</div>
+        <div className={style.userHashTag}>{user.nickname}</div>
       </div>
       <FollowButton />
     </div>
