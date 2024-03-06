6c2a9fa06181356b3f4cbef8b42221e88e63d31a
Namhyun <jenple9709@gmail.com>
2024-03-06 21:52:25 +0900

    feat: UseInview code

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -4,10 +4,10 @@ import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
 import { getPostRecommends } from '../_lib/getPostRecommends';
 import Post from '../../_component/Post';
 import { Post as PostProps } from '@/app/model/Post';
-import { Fragment } from 'react';
-
+import { Fragment, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 export default function PostRecommends() {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery<
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery<
     PostProps[],
     object,
     InfiniteData<PostProps[]>,
@@ -22,12 +22,26 @@ export default function PostRecommends() {
     getNextPageParam: (lastpage) => lastpage.at(-1)?.postId,
   });
 
-  return data?.pages.map((page, i) => {
-    <Fragment key={i}>
-      {page.map((post) => (
-        <Post key={post.postId} post={post} />
-      ))}
-      ;
-    </Fragment>;
+  const { ref, inView } = useInView({
+    threshold: 0,
+    delay: 100,
   });
+
+  useEffect(() => {
+    if (inView) {
+      !isFetching && hasNextPage && fetchNextPage();
+    }
+  }, [inView, fetchNextPage, isFetching, hasNextPage]);
+  return (
+    <>
+      {data?.pages.map((page, i) => (
+        <Fragment key={i}>
+          {page.map((post) => (
+            <Post key={post.postId} post={post} />
+          ))}
+        </Fragment>
+      ))}
+      <div ref={ref} style={{ height: 50 }} />
+    </>
+  );
 }
