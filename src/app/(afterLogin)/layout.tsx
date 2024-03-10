ad1d09d798dec7417f37dd51b1030e481a6e40ab
Namhyun <jenple9709@gmail.com>
2024-03-10 20:50:28 +0900

    refactor: Pass session as prop to LogoutButton for improved state management

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -53,7 +53,7 @@ export default async function AfterLoginLayout({
                     </svg>
                   </Link>
                 </nav>
-                <LogoutButton />
+                <LogoutButton me={session} />
               </>
             )}
           </div>
