d91a67f2a631b53b931febaaa610d9481ffe366b
Namhyun <jenple9709@gmail.com>
2024-03-13 14:15:43 +0900

    feat: UseMutation in PostForm

--- src/app/(afterLogin)/home/_component/PostForm.tsx
+++ src/app/(afterLogin)/home/_component/PostForm.tsx
@@ -1,10 +1,10 @@
 'use client';
 import style from './postForm.module.css';
-import { useState, useRef, ChangeEventHandler, FormEventHandler } from 'react';
+import { useState, useRef, ChangeEventHandler, FormEvent } from 'react';
 import Image from 'next/image';
 import { Session } from '@auth/core/types';
 import TextareaAutosize from 'react-textarea-autosize';
-import { useQueryClient } from '@tanstack/react-query';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
 import { Post } from '@/app/model/Post';
 
 type Props = {
@@ -17,29 +17,25 @@ export default function PostForm({ me }: Props) {
     Array<{ dataUrl: string; file: File } | null>
   >([]);
   const queryClient = useQueryClient();
-  const onChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-    setContent(e.target.value);
-  };
-  const onSubmit: FormEventHandler = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('content', content);
-    previewImages.forEach((v) => {
-      v && formData.append('images', v.file);
-    });
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`,
-        {
-          method: 'POST',
-          credentials: 'include',
-          body: formData,
-        }
-      );
-      if (response.status === 201) {
-        setContent('');
-        setPreviewImages([]);
-        const newPost = await response.json();
+  const mutation = useMutation({
+    mutationFn: async (e: FormEvent) => {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append('content', content);
+      previewImages.forEach((v) => {
+        v && formData.append('images', v.file);
+      });
+      return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`, {
+        method: 'POST',
+        credentials: 'include',
+        body: formData,
+      });
+    },
+    async onSuccess(response, variable) {
+      const newPost = await response.json();
+      setContent('');
+      setPreviewImages([]);
+      if (queryClient.getQueryData(['posts', 'followings'])) {
         queryClient.setQueryData(
           ['posts', 'recommends'],
           (prevData: { pages: Post[][] }) => {
@@ -49,28 +45,13 @@ export default function PostForm({ me }: Props) {
             };
             shallow.pages[0] = [...shallow.pages[0]];
             shallow.pages[0].unshift(newPost);
-            return prevData;
+            return shallow;
           }
         );
       }
-    } catch (err) {
-      alert('게시물을 작성하는 중 문제가 발생했습니다.');
-    }
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`,
-        {
-          method: 'POST',
-          credentials: 'include',
-          body: formData,
-        }
-      );
-      if (response.status === 201) {
-        setContent('');
-        setPreviewImages([]);
-        const newPost = await response.json();
+      if (queryClient.getQueryData(['posts', 'followings'])) {
         queryClient.setQueryData(
-          ['posts', 'followings'],
+          ['posts', 'recommends'],
           (prevData: { pages: Post[][] }) => {
             const shallow = {
               ...prevData,
@@ -78,13 +59,19 @@ export default function PostForm({ me }: Props) {
             };
             shallow.pages[0] = [...shallow.pages[0]];
             shallow.pages[0].unshift(newPost);
-            return prevData;
+            return shallow;
           }
         );
       }
-    } catch (err) {
-      alert('게시물을 작성하는 중 문제가 발생했습니다.');
-    }
+    },
+    onError(error) {
+      console.error(error);
+      alert('업로드 중 에러가 발생했습니다.');
+    },
+  });
+
+  const onChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setContent(e.target.value);
   };
 
   const onClickButton = () => {
@@ -114,7 +101,7 @@ export default function PostForm({ me }: Props) {
   };
 
   return (
-    <form className={style.postForm} onSubmit={onSubmit}>
+    <form className={style.postForm} onSubmit={mutation.mutate}>
       <div className={style.userSection}>
         <div className={style.userImage}>
           <Image
