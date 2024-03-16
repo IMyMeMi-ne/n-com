fc6d84a612d34eb3824ea0090243d3caba01ded1
Namhyun <jenple9709@gmail.com>
2024-03-16 13:45:33 +0900

    feat: Add session in username page.tsx

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -8,12 +8,14 @@ import {
 } from '@tanstack/react-query';
 import UserPosts from './_component/UserPosts';
 import UserInfo from './_component/UserInfo';
+import { auth } from '@/auth';
 
 type Props = {
   params: { username: string };
 };
 export default async function Profile({ params }: Props) {
   const { username } = params;
+  const session = await auth();
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: ['users', username],
@@ -28,7 +30,7 @@ export default async function Profile({ params }: Props) {
   return (
     <main className={style.main}>
       <HydrationBoundary state={dehydratedState}>
-        <UserInfo username={username} />
+        <UserInfo username={username} session={session} />
         <div>
           <UserPosts username={username} />
         </div>
