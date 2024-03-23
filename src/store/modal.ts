9584e897983a178cd0e402232eaf5e4296fdd3b0
Namhyun <jenple9709@gmail.com>
2024-03-23 13:18:37 +0900

    feat: Implement modal.ts

---
+++ src/store/modal.ts
@@ -0,0 +1,25 @@
+import { Post } from '@/app/model/Post';
+import { create } from 'zustand';
+
+interface modalState {
+  mode: 'new' | 'comment';
+  data: Post | null;
+  setMode(mode: 'new' | 'comment'): void;
+  setData(data: Post): void;
+}
+export const useModalStore = create<modalState>((set) => ({
+  mode: 'new',
+  data: null,
+  setMode(mode) {
+    set({ mode });
+  },
+  setData(data) {
+    set({ data });
+  },
+  reset() {
+    set({
+      mode: 'new',
+      data: null,
+    });
+  },
+}));
